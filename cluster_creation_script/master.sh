1cb3c83985cb13b9708b75f7e220e3b995edf0b3
Benker <lyleungad@connect.ust.hk>
2019-12-04 12:09:05 +0800

    move to one line

--- cluster_creation_script/master.sh
+++ cluster_creation_script/master.sh
@@ -14,17 +14,9 @@ newgrp docker
 
 # install docker
 sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
