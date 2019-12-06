89c4c7c4e7d5d5ceec9417081fd1bdb37eefa35d
Elliot Ng <chihimng.io@gmail.com>
2019-12-06 17:47:40 +0800

    feat: start minikube in script

--- lambda/bootstrap_dev_env.sh
+++ lambda/bootstrap_dev_env.sh
@@ -2,15 +2,20 @@
 
 # README
 # Bootstrap script for setting up development environment
-# Prerequisites: kubernetes and k3sup
+# Prerequisites: minikube and k3sup
 
 # exit if error occurs
 set -e
 
-# install openfaas
-echo "Installing openfaas..."
-k3sup app install openfaas # installs into openfaas and openfaas-fn namespaces
+# start minikube
+echo "> Starting minikube..."
+minikube start
 
 # install mongodb
-echo "Installing mongodb..."
+echo "> Installing mongodb..."
 k3sup app install chart --repo-name stable/mongodb --namespace mongodb
+
+# install openfaas
+echo "> Installing openfaas..."
+k3sup app install openfaas # installs into openfaas and openfaas-fn namespaces
+echo "> Follow instructions above to access openfaas"
