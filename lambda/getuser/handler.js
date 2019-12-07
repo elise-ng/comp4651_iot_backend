5731096272177a0e358f641471adea5323b60af2
Elliot Ng <chihimng.io@gmail.com>
2019-12-08 03:51:05 +0800

    feat: implement getuser, fix: update param format

--- lambda/getuser/handler.js
+++ lambda/getuser/handler.js
@@ -6,35 +6,34 @@ const MongoURL = process.env.mongo_url
 const MongoDBName = process.env.mongo_dbname
 
 /**
- * Lambda function for updating record of a user
- * Usage: input an user object with id and fields to be updated
+ * Lambda function for getting user by id
+ * Usage: input an user object with id field
  */
 
 module.exports = async (event, context) => {
   const client = new MongoClient(MongoURL)
   try {
     // input validation
-    const user = event.body
-    if (!user) { throw new HttpError(400, 'user object is empty') }
-    if (!user.id) { throw new HttpError(400, 'user id is empty') }
+    const id = event.body.id
+    if (!id) { throw new HttpError(400, 'user id is empty') }
 
     // upsert record on db
     await client.connect()
     const collection = client.db(MongoDBName).collection('users')
-    const result = await collection.updateOne(
-      { id: user.id },
-      { $set: user }
-    )
+    const result = await collection.find(
+      { id }
+    ).project({ _id: 0 }).limit(1).toArray()
     client.close()
 
-    if (result.matchedCount !== 1) { throw new HttpError(404, 'user not found') }
+    if (result.length < 1) { throw new HttpError(404, 'user not found') }
 
     // success response
     return context
       .status(200)
       .headers({ 'Content-Type': 'application/json' })
       .succeed(JSON.stringify({ // force response to be in json as auto content type is not reliable
-        success: true
+        success: true,
+        user: result[0]
       }))
   } catch (e) {
     // error response
